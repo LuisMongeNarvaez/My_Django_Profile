cd5d0ab834997facef52e542f911864647522fa8
TINTINCR1 <tintincr1@gmail.com>
2025-08-16 16:08:37 -0400

    Updated home.html with new profile layout

--- my_django_profile/views.py
+++ my_django_profile/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
 
 def home(request):
-    return render(request, 'my_django_profile/home.html')
+    return render(request, 'my_django_profile/home.html', {"name": "Luis"})
 
